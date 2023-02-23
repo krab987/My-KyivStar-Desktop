23d478c889bb4cae3add4ec0a3d972ae79bb77b9
VitaliiKrab <palochkin99@gmail.com>
2023-02-23 22:35:57 +0200

    a bit

--- KyivStar_Control/Services.cs
+++ KyivStar_Control/Services.cs
@@ -33,26 +33,26 @@ namespace KyivStar_Control
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
-            this.label15 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
-            this.label17 = new System.Windows.Forms.Label();
-            this.label18 = new System.Windows.Forms.Label();
-            this.label19 = new System.Windows.Forms.Label();
-            this.label20 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -99,15 +99,96 @@ namespace KyivStar_Control
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1006, 534);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // label1
+            // label20
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 80);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Антивизначник номера\r\nПриховайте свій  номер коли телефонуєте на інші мобільні\r\n1" +
-    ",65 грн/день";
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(756, 212);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(58, 20);
+            this.label20.TabIndex = 19;
+            this.label20.Text = "label20";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(505, 212);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(58, 20);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "label19";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(254, 212);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(58, 20);
+            this.label18.TabIndex = 17;
+            this.label18.Text = "label18";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(3, 212);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(58, 20);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "label17";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(756, 106);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 20);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "label12";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(505, 106);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(133, 40);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Польща без меж\r\nПакет для Польщі";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(254, 106);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(239, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Блокування інтернету в роумінгу";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 106);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 40);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Простий роумінг\r\nСпеціальні умови";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(756, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 60);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Екстра гроші\r\nОтримаєте поповнення бонустного рахунку\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 40);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Пошук дітей\r\n0 грн";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
@@ -119,23 +200,15 @@ namespace KyivStar_Control
             this.label2.Text = "Супервизначник номера\r\nДізнайтеся хто телефонує, навіть якщо номер приховано\r\n3 г" +
     "рн/день";
             // 
-            // label3
+            // label1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(505, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 40);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Пошук дітей\r\n0 грн";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(756, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(179, 60);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Екстра гроші\r\nОтримаєте поповнення бонустного рахунку\r\n";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 80);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Антивизначник номера\r\nПриховайте свій  номер коли телефонуєте на інші мобільні\r\n1" +
+    ",65 грн/день";
             // 
             // label5
             // 
@@ -173,59 +246,14 @@ namespace KyivStar_Control
             this.label8.TabIndex = 7;
             this.label8.Text = "label8";
             // 
-            // label9
+            // label16
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(3, 106);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(133, 40);
-            this.label9.TabIndex = 8;
-            this.label9.Text = "Простий роумінг\r\nСпеціальні умови";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(254, 106);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(239, 20);
-            this.label10.TabIndex = 9;
-            this.label10.Text = "Блокування інтернету в роумінгу";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(505, 106);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(133, 40);
-            this.label11.TabIndex = 10;
-            this.label11.Text = "Польща без меж\r\nПакет для Польщі";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(756, 106);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(58, 20);
-            this.label12.TabIndex = 11;
-            this.label12.Text = "label12";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(3, 424);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(58, 20);
-            this.label13.TabIndex = 12;
-            this.label13.Text = "label13";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(254, 424);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(58, 20);
-            this.label14.TabIndex = 13;
-            this.label14.Text = "label14";
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(756, 424);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(58, 20);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "label16";
             // 
             // label15
             // 
@@ -236,50 +264,23 @@ namespace KyivStar_Control
             this.label15.TabIndex = 14;
             this.label15.Text = "label15";
             // 
-            // label16
+            // label14
             // 
-            this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(756, 424);
-            this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(58, 20);
-            this.label16.TabIndex = 15;
-            this.label16.Text = "label16";
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(254, 424);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(58, 20);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "label14";
             // 
-            // label17
+            // label13
             // 
-            this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(3, 212);
-            this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(58, 20);
-            this.label17.TabIndex = 16;
-            this.label17.Text = "label17";
-            // 
-            // label18
-            // 
-            this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(254, 212);
-            this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(58, 20);
-            this.label18.TabIndex = 17;
-            this.label18.Text = "label18";
-            // 
-            // label19
-            // 
-            this.label19.AutoSize = true;
-            this.label19.Location = new System.Drawing.Point(505, 212);
-            this.label19.Name = "label19";
-            this.label19.Size = new System.Drawing.Size(58, 20);
-            this.label19.TabIndex = 18;
-            this.label19.Text = "label19";
-            // 
-            // label20
-            // 
-            this.label20.AutoSize = true;
-            this.label20.Location = new System.Drawing.Point(756, 212);
-            this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(58, 20);
-            this.label20.TabIndex = 19;
-            this.label20.Text = "label20";
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(3, 424);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(58, 20);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "label13";
             // 
             // Services
             // 
@@ -290,6 +291,11 @@ namespace KyivStar_Control
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
 
         }
     }
